7474c97fb9e36bfc28dd93685b91ebb23e750e0d
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2023-02-07 17:41:32 +0530

    Update DemoSubprojectK.java

--- subproject/src/main/java/subproject/DemoSubprojectK.java
+++ subproject/src/main/java/subproject/DemoSubprojectK.java
@@ -1,1 +1,31 @@
+package subproject;
 
+public class DemoSubprojectK {
+
+	public static void main(String[] args) { 
+		int k_subproject;   
+		
+	}
+	
+	private static DemoSubproject foo = null;
+	
+	
+	//multiple simultaneous callers may see partially initialized objects
+
+ public static DemoSubproject getFoo() { 
+	    System.out.println("");   
+	    System.out.println("");
+	    System.out.println(""); 
+	    System.out.println("");
+	    System.out.println("");
+	    System.out.println("");
+ }
+
+ public static DemoSubproject getFoo2() {
+     if (foo==null) {
+         foo = new DemoSubproject();
+     }
+    return foo;
+ }
+ 
+}
